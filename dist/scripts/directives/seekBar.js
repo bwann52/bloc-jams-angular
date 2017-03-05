b777a51cb460dd7050884b793b9097eeecfc1893
Bauer <bwann52@users.noreply.github.com>
2017-03-05 14:52:01 -0800

    cp9

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -1,5 +1,14 @@
 (function() {
-     function seekBar() {
+     function seekBar($document) {
+         
+         var calculatePercent = function(seekBar, event) {
+             var offsetX = event.pageX - seekBar.offset().left;
+             var seekBarWidth = seekBar.width();
+             var offsetXPercent = offsetX / seekBarWidth;
+             offsetXPercent = Math.max(0, offsetXPercent);
+             offsetXPercent = Math.min(1, offsetXPercent);
+             return offsetXPercent;
+        };
          
         return {
              templateUrl: '/templates/directives/seek_bar.html',
@@ -9,6 +18,8 @@
              link: function(scope, element, attributes) {
                  scope.value = 0;
                  scope.max = 100;
+                 
+                 var seekBar = $(element);
 
                  var percentString = function () {
                      var value = scope.value;
@@ -20,6 +31,26 @@
                  scope.fillStyle = function() {
                      return {width: percentString()};
                  };
+                 
+                 scope.onClickSeekBar = function(event) {
+                     var percent = calculatePercent(seekBar, event);
+                     scope.value = percent * scope.max;
+                 };
+                 
+                 
+                 scope.trackThumb = function() {
+                     $document.bind('mousemove.thumb', function(event) {
+                         var percent = calculatePercent(seekBar, event);
+                         scope.$apply(function() {
+                             scope.value = percent * scope.max;
+                         });
+                     });
+
+                     $document.bind('mouseup.thumb', function() {
+                         $document.unbind('mousemove.thumb');
+                         $document.unbind('mouseup.thumb');
+                     });
+                 };
              }
         };
          
@@ -27,5 +58,5 @@
  
      angular
          .module('blocJams')
-         .directive('seekBar', seekBar);
+         .directive('seekBar', ['$document', seekBar]);
  })();
